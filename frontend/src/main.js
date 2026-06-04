d6aa73a04b877f02e92622fdb6e23157278c1a14
HaoCheng-Wang <hcwang0025@163.com>
2026-06-05 01:14:52 +0800

    Add virtual scrolling and extract RequestCard component for better performance

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueVirtualScroller from 'vue-virtual-scroller'
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
 import App from './App.vue'
 import router from './router'
 import './style.css'
@@ -7,4 +9,5 @@ import './style.css'
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+app.use(VueVirtualScroller)
 app.mount('#app')
